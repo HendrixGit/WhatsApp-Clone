2013b3f0916da43beaa6c9b00fa8c3102f7c1c81
Bruno <brunosalustiano303@hotmail.com>
2017-12-13 01:26:33 -0200

    Dialog adicionar contato

--- app/src/main/java/whatsapp/cursoandroid/com/whatsapp/activity/MainActivity.java
+++ app/src/main/java/whatsapp/cursoandroid/com/whatsapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package whatsapp.cursoandroid.com.whatsapp.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -65,9 +68,36 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.item_sair : deslogarUsuario(); return true;
             case R.id.item_configuracoes: return true;
+            case R.id.item_adicionar: abrirCadastroContato(); return true;
             default: return super.onOptionsItemSelected(item);
         }
     }
+
+    private void abrirCadastroContato(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Novo Contato");
+        alertDialog.setMessage("Telefone: ");
+        alertDialog.setCancelable(false);// nao e possivel fechar a janela ao clicar fora
+
+        EditText editText = new EditText(getApplicationContext());
+        alertDialog.setView(editText);
+
+        alertDialog.setPositiveButton("Cadastrar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.create();
+        alertDialog.show();
+    }
+
     public void deslogarUsuario(){
         autenticacao.signOut();
         Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
