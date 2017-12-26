1bc0a3e9555d122af122d8f3cc09375312c42da6
Bruno <brunosalustiano303@hotmail.com>
2017-12-26 16:17:02 -0200

    Data e hora nas mensagens

--- app/src/main/java/whatsapp/cursoandroid/com/whatsapp/adapter/MensagemAdapter.java
+++ app/src/main/java/whatsapp/cursoandroid/com/whatsapp/adapter/MensagemAdapter.java
@@ -7,10 +7,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 
 import whatsapp.cursoandroid.com.whatsapp.R;
 import whatsapp.cursoandroid.com.whatsapp.helper.Preferencias;
+import whatsapp.cursoandroid.com.whatsapp.helper.Util;
 import whatsapp.cursoandroid.com.whatsapp.model.Mensagem;
 
 public class MensagemAdapter extends ArrayAdapter {
@@ -38,11 +40,19 @@ public class MensagemAdapter extends ArrayAdapter {
 
             if(idUsuarioRemetente.equals(mensagem.getIdUsuario())){
                 view = inflater.inflate(R.layout.item_mensagem_direita, parent, false);
-            }else{
+            }
+            else{
                 view = inflater.inflate(R.layout.item_mensagem_esquerda, parent, false);
             }
             TextView textoMensagem = (TextView) view.findViewById(R.id.tv_mensagem);
+            TextView textoHoraData = (TextView) view.findViewById(R.id.tv_horamensagem);
             textoMensagem.setText(mensagem.getMensagem());
+            Util util = new Util();
+            try {
+                textoHoraData.setText(util.returnDataString(mensagem.getData()));
+            } catch (ParseException e) {
+
+            }
         }
 
         return view;
